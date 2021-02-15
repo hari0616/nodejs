d53367acd0555063dbf5af15e4a853a21bbad81b
hari0616 <47607160+hari0616@users.noreply.github.com>
2021-02-15 18:37:54 +0530

    week2

---
+++ stack.js
@@ -0,0 +1,9 @@
+//
+const oranges = ['orange', 'orange','guva','apple']
+const apples = ['just one apple','apple']
+oranges.forEach(fruit => {
+  console.count(fruit)
+})
+apples.forEach(fruit => {
+  console.count(fruit)
+})
